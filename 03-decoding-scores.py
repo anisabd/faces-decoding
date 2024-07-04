bd37b887bc9658758392df43e8c38ad29121c657
anisabd <148235475+anisabd@users.noreply.github.com>
2024-07-04 11:24:10 -0400

    Update 03-decoding-scores.py
    Updated version of script that works for Compute Canada

--- 03-decoding-scores.py
+++ 03-decoding-scores.py
@@ -1,4 +1,4 @@
-import os.path as op
+import os
 import mne
 from mne.parallel import parallel_func
 from mne.decoding import (
@@ -21,6 +21,10 @@ from sklearn.neighbors import KNeighborsClassifier as KNN
 from sklearn.neural_network import MLPClassifier
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
 
 
 def read_sub_epochs(subject_id: int, freq_band: str = None) -> mne.Epochs:
@@ -37,11 +41,11 @@ def read_sub_epochs(subject_id: int, freq_band: str = None) -> mne.Epochs:
     subject = f"sub-{subject_id:02d}"
     if freq_band is None:
         epochs = mne.read_epochs(
-            op.join(meg_dir, subject, f"{subject}-epo.fif"), verbose=False
+            os.path.join(meg_dir, subject, f"{subject}-epo.fif"), verbose=False
         )
     else:
         epochs = mne.read_epochs(
-            op.join(meg_dir, subject, f"{subject}-{freq_band}-epo.fif"), verbose=False
+            os.path.join(meg_dir, subject, f"{subject}-{freq_band}-epo.fif"), verbose=False
         )
     return epochs
 
@@ -72,7 +76,7 @@ def run_spatiotemp_decoder_single_sub(subject_id:int):
                 epochs = read_sub_epochs(subject_id)
             else:
                 epochs = read_sub_epochs(subject_id, freq_band)
-            print(f"  Modality: {modality}, Frequency band: {freq_band}")
+            logger.info(f"  Modality: {modality}, Frequency band: {freq_band}")
             
 
             if modality == "face_vs_scrambled":
@@ -109,11 +113,11 @@ def run_spatiotemp_decoder_single_sub(subject_id:int):
                 # Mean scores across cross-validation splits
                 score = np.mean(scores, axis=0)
                 return score
-            print(f"  Running spatio-temporal decoding analysis for subject {subject}, modality {modality} and frequency band {freq_band}...") 
+            logger.info(f"  Running spatio-temporal decoding analysis for subject {subject}, modality {modality} and frequency band {freq_band}...") 
             score = clv_spatio_temporal(X, y)
             scores_all.append([subject, modality, freq_band, score])
     scores_all_df = pd.DataFrame(scores_all, columns=["Subject", "Modality", "Frequency Band", "Score"])
-    print('Saving all scores to csv file...')
+    logger.info('Saving all scores to csv file...')
     scores_all_df.to_csv(f"scores_decoding_svm_rbf_spatiotemporal_sub-{subject_id:02d}.csv", index=False)
 
 # Leave one subject out decoding analysis
@@ -123,11 +127,11 @@ def run_spatiotemp_decoder_LOSOCV(subject_ids:list[int]):
 
     # Iterate over all subjects considering them as testing split
     for test_subject_id in subject_ids:
-        print(f"Starting cross-validation with testing subject {test_subject_id:02d}")
+        logger.info(f"Starting cross-validation with testing subject {test_subject_id:02d}")
           
         test_sub = f"sub-{test_subject_id:02d}"
         
-        
+        train_subjects = subject_ids.copy()
         train_subjects.remove(test_subject_id) # No need for the for loop
         
         # Iterate over both modalities 
@@ -136,11 +140,11 @@ def run_spatiotemp_decoder_LOSOCV(subject_ids:list[int]):
             # Define the model each time we change modality
             model = SVC(kernel='linear', C=1, max_iter=1000)
             
-            print(f"Modality: {modality}")
+            logger.info(f"Modality: {modality}")
             X, y = [], []
             # Iterate over all subjects except the testing subject to train the model
             for train_sub_id in train_subjects:
-                epochs = mne.read_epochs(op.join(meg_dir, f'sub-{train_sub_id:02d}', f'sub-{train_sub_id:02d}-epo.fif'), verbose=False)
+                epochs = mne.read_epochs(os.path.join(meg_dir, f'sub-{train_sub_id:02d}', f'sub-{train_sub_id:02d}-epo.fif'), verbose=False)
                 
                 if modality == "face_vs_scrambled":
                     events = mne.merge_events(epochs.events, [1,2], 1, replace_events=True)
@@ -153,6 +157,10 @@ def run_spatiotemp_decoder_LOSOCV(subject_ids:list[int]):
                 y_subj = events[:, 2]
                 X.append(X_subj)
                 y.append(y_subj)
+                print(len(X), len(y))
+            # Flatten the list of arrays
+            X = [epoch for subject in X for epoch in subject]
+            y = [epoch for subject in y for epoch in subject]
             X = np.array(X)
             y = np.array(y)
             clf = make_pipeline(
@@ -162,8 +170,8 @@ def run_spatiotemp_decoder_LOSOCV(subject_ids:list[int]):
             )
             clf.fit(X, y) # You fit the model on 15 subject simultaniously and test on 1 subject 
             # print(f"Fitted model for modality {modality} and subject {train_sub_id}")
-            print(f"Finished training for modality {modality}, now testing on subject {test_sub}")
-            epochs = mne.read_epochs(op.join(meg_dir, test_sub, f'{test_sub}-epo.fif'), verbose=False)
+            logger.info(f"Finished training for modality {modality}, now testing on subject {test_sub}")
+            epochs = mne.read_epochs(os.path.join(meg_dir, test_sub, f'{test_sub}-epo.fif'), verbose=False)
             if modality == 'face_vs_scrambled':
                 events = mne.merge_events(epochs.events, [1,2], 1, replace_events=True)
             else:
@@ -187,7 +195,7 @@ def run_spatiotemp_decoder_LOSOCV(subject_ids:list[int]):
     {"Modality": "Scores Famous vs Unfamiliar", "Score": score_fvu}
     ]
     scores_all_df = pd.DataFrame(scores_all)
-    print('Saving all scores to csv file...')
+    logger.info('Saving all scores to csv file...')
     scores_all_df.to_csv(f"scores_decoding_svm_rbf_spatiotemporal_LOSOCV.csv", index=False)
 
 
@@ -196,4 +204,4 @@ def run_spatiotemp_decoder_LOSOCV(subject_ids:list[int]):
 #parallel, run_func, _ = parallel_func(run_spatiotemp_decoder_single_sub, n_jobs=-1)
 #parallel(run_func(subject_id) for subject_id in range(1, 2))
 
-run_spatiotemp_decoder_LOSOCV(list(range(1, 16)))
+run_spatiotemp_decoder_LOSOCV(list(range(1, 17)))
